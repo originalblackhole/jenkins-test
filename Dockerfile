6976ec892176b9b5119296c82a04ef3062e36d81
maketozero <makezero@yeah.net>
2021-03-13 04:01:50 +0800

    first commit

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# 下载maven与java的镜像
+FROM hub.c.163.com/wuxukun/maven-aliyun:3-jdk-8
+# 指定当前操作目录
+WORKDIR /usr/local/jenkins/jenkins-test
+#指定对外端口号
+EXPOSE 7777
+#启动java程序
+#--spring.profiles.active=dev 多环境下指定环境 。 -c为清除以前启动的数据
+ENTRYPOINT ["java","-jar","jenkins-test-0.0.1-SNAPSHOT.jar"]
---
+++ docker.sh
@@ -0,0 +1,15 @@
+BASE_PATH=/usr/local/jenkins/jenkins-test
+# 源jar路径  即jenkins构建后存放的路径
+SOURCE_PATH=/home/jenkins/workspace
+#docker 镜像/容器名字或者jar名字 这里都命名为这个
+SERVER_NAME=jenkins-test
+#容器id
+CID=$(docker ps | grep "$SERVER_NAME" | awk '{print $1}')
+#镜像id
+IID=$(docker images | grep "$SERVER_NAME" | awk '{print $3}')
+
+if [ -n "$CID" ] ;then
+	echo "存在$SERVER_NAME容器，CID=$CID"
+	docker stop $SERVER_NAME
+fi
+sudo docker run -u root --rm -d -p 8761:8761 --name $SERVER_NAME --privileged=true -v $BASE_PATH:$BASE_PATH jenkins/test:latest
