a9f41d7b372b59f68d2ad3b4730471646355a164
maketozero <makezero@yeah.net>
2021-03-13 23:00:29 +0800

    update jenkinsfile

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,19 +1,30 @@
 node {
+
     //定义mvn环境
     def mvnHome = tool 'M3'
     env.PATH = "${mvnHome}/bin:${env.PATH}"
+
     stage('build') {
+
         checkout scm
         sh 'mvn -B -DskipTests clean package'
+
     }
 
     stage('docker build') {
-        def customImage = docker.build("jenkins-test:${env.BUILD_ID}")
-        sh "echo ${env.BUILD_ID}"
+
+        // 停止容器
+        sh "docker stop $(docker ps | grep jenkins-test | awk '{print $1}')"
+        // 删除旧镜像
+        sh "docker rmi $(docker images | grep jenkins-test | awk '{print $1}')"
+        //构建镜像
+        def customImage = docker.build("jenkins-test:latest")
+
     }
 
     stage("deploy") {
-        sh "pwd"
+
         sh ' ./deploy.sh'
+
     }
 }
--- deploy.sh
+++ deploy.sh
@@ -6,20 +6,4 @@ BASE_PATH=/usr/local/jenkins/$SERVER_NAME
 SOURCE_PATH=/home/jenkins/workspace/$SERVER_NAME
 #docker 镜像/容器名字或者jar名字 这里都命名为这个
 
-#容器id
-CID=$(docker ps | grep "$SERVER_NAME" | awk '{print $1}')
-#镜像id
-IID=$(docker images | grep "$SERVER_NAME" | awk '{print $3}')
-
-if [ -n "$CID" ] ;then
-	echo "存在$SERVER_NAME容器，CID=$CID"
-	docker stop $SERVER_NAME
-	docker rm $SERVER_NAME
-
-fi
-#docker run -u root --rm -d -p 7777:7777 --name $SERVER_NAME --privileged=true -v $BASE_PATH:$BASE_PATH jenkins/test:latest
-#docker run -u root --rm -d -p 7777:7777 --name jenkins-test --privileged=true  jenkins/test:latest
-docker run --name $SERVER_NAME -d -p 7777:7777 --privileged=true -v $BASE_PATH:$BASE_PATH $SERVER_NAME
-
-# 删除不运行的镜像
-docker rmi $(IID)
+docker run --name $SERVER_NAME --rm -d -p 7777:7777 --privileged=true -v $BASE_PATH:$SOURCE_PATH $SERVER_NAME
