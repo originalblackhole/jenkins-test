f0a0ecf52576ce38b89f313be1a5f1c0cbadd5b1
maketozero <makezero@yeah.net>
2021-03-14 09:21:49 +0800

    update jenkinsfile

--- delete.sh
+++ delete.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 # 停止容器   删除非运行的容器 docker rm `docker ps -a -q`  删除docker无引用的镜像 docker rmi -f `docker images | grep'<none>' | awk '{print $3}'`
-CID=$(docker ps | grep jenkins-test | awk '{print $1}')
+CID=$(docker ps -a | grep jenkins-test | awk '{print $1}')
 if [ -n "$CID" ] ;then
     docker stop $CID
+    docker rm $CID
 fi
 
 
